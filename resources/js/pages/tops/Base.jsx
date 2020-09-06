104387b161bcc7eea787b3f964a183f34ee19b60
wollllll <sakin50guramu@gmail.com>
2020-09-07 05:09:40 +0900

    posts seeder 作成

--- resources/js/pages/tops/Base.jsx
+++ resources/js/pages/tops/Base.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import ReactDOM from "react-dom";
 import Index from "../../pages/tops/Index";
+import NotFound from "../../components/NotFound";
 
 export const Base = () => {
     return (
         <Router>
             <Switch>
                 <Route exact path="/" component={Index}/>
+                <Route component={NotFound}/>
             </Switch>
         </Router>
     );
